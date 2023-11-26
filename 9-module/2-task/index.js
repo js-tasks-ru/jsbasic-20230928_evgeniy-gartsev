8c6252c36b3f2ad54b6319c04a937c0509a83529
Evgeniy <icancan1414@gmail.com>
2023-11-26 20:01:59 +0300

    Completed 9/2

--- 9-module/2-task/index.js
+++ 9-module/2-task/index.js
@@ -16,6 +16,73 @@ export default class Main {
   }
 
   async render() {
-    // ... ваш код
+    this.carousel = new Carousel(slides);
+    this.ribbonMenu = new RibbonMenu(categories);
+    this.stepSlider = new StepSlider({steps: 5, value: 3});
+    this.cartIcon = new CartIcon();
+    this.cart = new Cart(this.cartIcon);
+    this.noNutsCheckbox = document.querySelector("#nuts-checkbox");
+    this.vegeterianCheckbox = document.querySelector("#vegeterian-checkbox");
+
+    document.querySelector("[data-carousel-holder]").insertAdjacentElement("afterbegin", this.carousel.elem);
+    document.querySelector("[data-ribbon-holder]").insertAdjacentElement("afterbegin", this.ribbonMenu.elem);
+    document.querySelector("[data-slider-holder]").insertAdjacentElement("afterbegin", this.stepSlider.elem);
+    document.querySelector("[data-cart-icon-holder]").insertAdjacentElement("afterbegin", this.cartIcon.elem);
+
+    const response = await fetch("products.json");
+    this.products = await response.json();
+    const dataProductsGridHolder = document.querySelector("[data-products-grid-holder]");
+
+    this.productsGrid = new ProductsGrid(this.products);
+
+    this.filterProductsGrid({
+      noNuts: this.noNutsCheckbox.checked,
+      vegeterianOnly: this.vegeterianCheckbox.checked,
+      maxSpiciness: this.stepSlider.elem.value,
+      category: this.ribbonMenu.elem.value
+    });
+
+    dataProductsGridHolder.innerHTML = "";
+    dataProductsGridHolder.insertAdjacentElement("afterbegin", this.productsGrid.elem);
+
+    this.addEvents();
+    return new Promise(resolve => {
+      resolve();
+    });
+  }
+
+  filterProductsGrid(filter) {
+    this.productsGrid.updateFilter(filter);
+  }
+
+  addEvents() {
+    this.ribbonMenu.elem.addEventListener("ribbon-select", event => {
+      this.filterProductsGrid({
+        category: event.detail
+      });
+    });
+
+    this.stepSlider.elem.addEventListener("slider-change", event => {
+      this.filterProductsGrid({
+        maxSpiciness: event.detail
+      });
+    });
+
+    this.noNutsCheckbox.addEventListener("change", event => {
+      this.filterProductsGrid({
+        noNuts: event.target.checked
+      });
+    });
+
+    this.vegeterianCheckbox.addEventListener("change", event => {
+      this.filterProductsGrid({
+        vegeterianOnly: event.target.checked
+      });
+    });
+
+    document.body.addEventListener("product-add", event => {
+      const product = this.products.filter(product => product.id == event.detail);
+      this.cart.addProduct(product[0]);
+    });
   }
 }
